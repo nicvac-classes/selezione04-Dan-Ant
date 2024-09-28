9054b719dbdc12d0e31b0d05907323a6640956ea
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-09-28 08:40:13 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    float n1, n2, n3, max;
+    cout << "Qual è il primo numero?" << endl;
+    cin >> n1;
+    cout << "Qual è il secondo numero?" << endl;
+    cin >> n2;
+    cout << "Qual è il terzo numero?" << endl;
+    cin >> n3;
+if (n1>n2)
+{
+    if (n1>n3)
+    {
+        max = n1;
+    }
+    else
+    {
+        max = n3;
+    }
+
+}
+else 
+{
+    if (n2 > n3)
+    {
+        max = n2;
+    }
+    else
+    {
+        max = n3;
+    }
+}
+    cout << "Il numero massimo è " << max;
+
+
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
